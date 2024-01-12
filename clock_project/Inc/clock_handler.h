98f95f2c3771a04e3a46c7c7853e0bd0fe6a1f87
Jourdan Rampoldi <104399321+Jrampoldi@users.noreply.github.com>
2024-01-12 14:16:22 -0700

    Update clock_handler.h
    Function descriptions updated.

--- clock_project/Inc/clock_handler.h
+++ clock_project/Inc/clock_handler.h
@@ -11,9 +11,18 @@
 #ifndef CLOCK_HANDLER_H_
 #define CLOCK_HANDLER_H_
 
+/* Function Declarations*/
 int calculate_min(int s);
+// Description: takes in seconds as an argument then
+// returns calculated minutes.
 int calculate_hour(int s);
+// Description: takes in seconds as an argument then
+// returns calculated hours
 int calculate_seconds_rem(int s);
+// Description: takes in seconds as an argument then
+// returns the calculated remaining seconds 
+// after breaking them down into hours and minutes
+
 void set_time(int h, int m, int s);
 
 #endif
